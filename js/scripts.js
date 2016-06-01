329619e00ec31e7b8488b2badb2b73d4f3214fb8
leah nelson and maarja laineste <maarja@>
2016-06-01 13:48:01 -0700

    add css formatting to survey page, bootstrap alert, jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,17 @@ $(function() {
 
     var dob = $("#born").val();
     $(".born").text(dob);
-    
+
     var meal = $("input:radio[name=meal]:checked").val();
     $(".meal").text(meal);
 
     var music = $("#music").val();
     $(".music").text(music);
 
+    var favoriteColor = $("#color").val();
+    $(".color").text(favoriteColor);
+
+    $(".alert").show();
     $("#survey-results").show();
     event.preventDefault();
   });
